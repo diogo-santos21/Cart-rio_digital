03a8c4f06f6848b5656aedeee7c6d8aaeffd0d7f
Diogo dos Santos <diogodisant@gmail.com>
2024-09-01 21:57:25 -0300

    Add files via upload

---
+++ exercicio.js
@@ -0,0 +1,13 @@
+function armazenarValor(){
+
+var userInput = document.getElementById("userInput").value;
+
+    document.getElementById("valorInserido").innerText = "Nome: " + userInput; 
+
+}
+
+function armazenarValor1(){
+    var userInput2 = document.getElementById("userInput2").value;
+    document.getElementById("valorInserido2").innerText = "Idade: " + userInput2;
+
+}
